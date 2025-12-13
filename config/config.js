8d386cecc9a57195065545d1aaaa1288c98a2899
sadiyabhbb <sadiyabhbb@proton.me>
2025-12-13 07:58:31 +0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "8561048024:AAEYJRLp3CWM5tjnX3MxyuCdTmN-RutRFbI",
+  BOT_TOKEN: "8561048024:AAGunN8YzGYpRZPt18n9O9DWDH3My5SSdSw",
 
   BOT_SETTINGS: {
     NAME: "𝄞⋆⃝🧚‍𝐁𝐀𝐃𝐎𝐋-𝐁𝐎𝐓🧚‍⋆⃝𝄞",
@@ -8,12 +8,20 @@ module.exports = {
   },
 
   OWNER: {
-    ID: "8287206585",
+    ID: 8287206585,
     USERNAME: "LIKHONAHMED009"
   },
 
   REQUIRED_CHATS: [
-    { id: "-1003319296127", name: "𝐋𝐈𝐊𝐇𝐎𝐍 𝐗 𝐁𝐎𝐎𝐌𝐒 𝐀𝐏𝐊 💀" },
-    { id: "-1002710357307", name: "✨⟦ 𝐗𝟐𝟎 - 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 ⟧✨" }
+    {
+      id: -1003319296127,
+      username: "likhon_premium",
+      name: "𝐋𝐈𝐊𝐇𝐎𝐍 𝐗 𝐁𝐎𝐎𝐌𝐒 𝐀𝐏𝐊 💀"
+    },
+    {
+      id: -1002710357307,
+      username: "likhon_x_booms_apk",
+      name: "✨⟦ 𝐗𝟐𝟎 - 𝐔𝐏𝐃𝐀𝐓𝐄𝐒 ⟧✨"
+    }
   ]
 };
