8e8585d32fdf0388d422f8063f26ce492dec06ba
sadiyabhbb <sadiyabhbb@proton.me>
2025-12-14 21:20:40 +0600

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  BOT_TOKEN: "8561048024:AAFG_NLWjMlhW7rHK-OoURQCHdDpfJ7W4io",
+  BOT_TOKEN: "8447574444:AAHxyNN-92b9N8BZl9JVamX_giIwT9TnFKI",
 
   BOT_SETTINGS: {
     NAME: "✨⟦ 𝐋𝐈𝐊𝐇𝐎𝐍 𝐁𝐎𝐓 ⟧✨",
